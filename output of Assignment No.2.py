32f9fe1d40b5cd4e7f7f9d58d372c95563636d5a
MADHAV <104258321+chaudharymadhav@users.noreply.github.com>
2022-05-06 09:39:51 +0530

    Add files via upload

---
+++ output of Assignment No.2.py
@@ -0,0 +1,46 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+C:/Users/mkcsn/PycharmProjects/python College Assignement/Assignment no.2.py
+SyntaxError: invalid syntax
+
+= RESTART: C:\Users\mkcsn\PycharmProjects\python College Assignement\Assignment no.2.py
+35
+egaugnal evitisnes esac a si nohtyP
+a case sensitive 
+Python is object oriented language
+10
+Pythonisacasesensitivelanguage
+Enter your good name
+Madhav
+Hey Madhav  Here!
+Enter your SID
+21107038
+My SID is  21107038
+Enter your Branch
+Mechanical
+Enter your CGPA
+10
+I am from  Mechanical department and my CGPA is 10
+The value of a&b =  8
+The value of a|b =  58
+The value of a^b =  50
+The value of a<<2 =  224 
+The value of b<<2 =  40
+The value of a>>2 =  14 
+The value of b>>4 =  0
+Enter any sentence of your choice:-
+Myself Madhav
+No
+This program determine if,the three sides entered by the use can form a triangle or not ?
+Enter first side of triangle:-
+3
+Enter second side of triangle:-
+4
+Enter third side of triangle:-
+5
+The sides entered by the use can form a triangle?
+The Answer is  YES
+Enter the value of a:-12
+Enter the value of b:-15
+1
+2
